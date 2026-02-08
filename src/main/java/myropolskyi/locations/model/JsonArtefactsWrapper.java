bc32f7c306df4d1e4eb999e66ee6720532947027
MyropolskyiH <hennadii.myropolskyi@bol-systemhaus.de>
2026-02-08 17:11:08 +0100

    release 2.2.2: JsonArtefactWrapper was refactored in order to use it in API without changes

--- src/main/java/myropolskyi/locations/model/JsonArtefactsWrapper.java
+++ src/main/java/myropolskyi/locations/model/JsonArtefactsWrapper.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * class for reading list of artefacts from json-file
@@ -15,24 +16,38 @@ import java.util.List;
 @NoArgsConstructor
 public class JsonArtefactsWrapper {
 
-    private String date;
-    private String subversion;
     private String thema;
     private String region;
     private String error;
     private Announcement announcement;
-    private List<Category> categories;
+    private Set<Category> categories;
     private List<Artefact> artefacts;
-    private List<WebAuthor> webAuthors;
+    private Set<WebAuthor> webAuthors;
+    private String version;
+    private String stringLocalDate;
 
-    public JsonArtefactsWrapper(String date, String subversion, String thema, String region, String error, List<Category> categories, List<Artefact> artefacts) {
-        this.date = date;
-        this.subversion = subversion;
+    public JsonArtefactsWrapper(List<Artefact> artefacts, Set<Category> categories, Set<WebAuthor> webAuthors) {
+        this.artefacts = artefacts;
+        this.categories = categories;
+        this.webAuthors = webAuthors;
+    }
+
+    public JsonArtefactsWrapper(String date,
+                                String version,
+                                String thema,
+                                String region,
+                                String error,
+                                Set<Category> categories,
+                                List<Artefact> artefacts,
+                                Set<WebAuthor> webAuthors) {
+        this.stringLocalDate = date;
+        this.version = version;
         this.thema = thema;
         this.region = region;
         this.error = error;
         this.categories = categories;
         this.artefacts = artefacts;
+        this.webAuthors = webAuthors;
     }
 
 }
