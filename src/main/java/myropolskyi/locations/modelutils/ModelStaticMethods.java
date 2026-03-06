3a94219e75dbe1218f0ea1ae2eeab8d2eafa97d9
MyropolskyiH <hennadii.myropolskyi@bol-systemhaus.de>
2026-03-06 15:16:33 +0100

    release 2.2.2 User-Agent

--- src/main/java/myropolskyi/locations/modelutils/ModelStaticMethods.java
+++ src/main/java/myropolskyi/locations/modelutils/ModelStaticMethods.java
@@ -121,7 +121,7 @@ public class ModelStaticMethods {
         StringBuilder result = new StringBuilder();
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
-        conn.setRequestProperty("User-Agent", "LookAroundArchitecture/2.5.3 (https://play.google.com/store/apps/details?id=myropolskyi.android.locations&pcampaignid=web_share), contact: Hennadii.Myropolskyi@outlook.com");
+        conn.setRequestProperty("User-Agent", "LookAroundArchitecture/3.0.0 (https://play.google.com/store/apps/details?id=myropolskyi.android.locations&pcampaignid=web_share), contact: Hennadii.Myropolskyi@outlook.com");
 
         int status = conn.getResponseCode();
         if (status == HttpURLConnection.HTTP_MOVED_TEMP
